9594ef7db961b9ad144be5e0705c6e8c3486c42b
nice77 <n.i.y.a.z.19831977@gmail.com>
2024-03-01 12:58:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.task
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
