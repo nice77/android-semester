eb59b90e4da6972a85ef68881c5b821f66821eff
nice77 <n.i.y.a.z.19831977@gmail.com>
2024-03-13 22:54:40 +0300

    Added viewBinding delegate

--- app/src/main/java/com/example/task/presentation/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/task/presentation/welcome/WelcomeFragment.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.task.R
 import com.example.task.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
-    private var binding : FragmentWelcomeBinding ?= null
+    private val binding : FragmentWelcomeBinding by viewBinding(FragmentWelcomeBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = FragmentWelcomeBinding.bind(view).also { binding ->
+        binding.run {
             binding.signInBtn.setOnClickListener {
                 findNavController().navigate(R.id.action_welcomeFragment_to_authFragment)
             }
