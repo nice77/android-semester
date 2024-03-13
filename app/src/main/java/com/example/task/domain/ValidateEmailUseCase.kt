bf6d9f13ebf81f2b9cad8d7c17bbfed0268bb425
nice77 <n.i.y.a.z.19831977@gmail.com>
2024-03-13 22:58:22 +0300

    Minor changes

--- app/src/main/java/com/example/task/domain/ValidateEmailUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.task.domain.validators
-
-class ValidateEmailUseCase {
-    operator fun invoke(email : String) : Boolean {
-        return email.matches(Regex("^\\S+@\\S+\\.\\S+\$"))
-    }
-}
