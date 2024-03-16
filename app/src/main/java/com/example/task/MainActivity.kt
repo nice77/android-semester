543c76e222545b0372449e7c463fc79221b7bcb7
nice77 <n.i.y.a.z.19831977@gmail.com>
2024-03-16 18:14:31 +0300

    Minor changes

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -3,17 +3,17 @@ package com.example.task
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.task.databinding.ActivityMainBinding
 import com.example.task.di.ServiceLocator
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private var binding : ActivityMainBinding ?= null
+    private val binding : ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ServiceLocator.setContext(applicationContext)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
                 as NavHostFragment
         val controller = navHostFragment.navController
