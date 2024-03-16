f651ccb96fadac399234186c7f2221d9df5b7832
nice77 <n.i.y.a.z.19831977@gmail.com>
2024-03-16 18:10:09 +0300

    Added kotlinx-serialization annotations

--- app/src/main/java/com/example/task/data/remote/datasource/requests/RegisterRequest.kt
+++ app/src/main/java/com/example/task/data/remote/datasource/requests/RegisterRequest.kt
@@ -1,7 +1,14 @@
 package com.example.task.data.remote.datasource.requests
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RegisterRequest(
+    @SerialName("name")
     val name : String,
+    @SerialName("email")
     val email : String,
+    @SerialName("password")
     val password : String
 )
